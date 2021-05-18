1a7874335d340939e5ff71b7030c4f81a73fd475
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-05-18 22:07:39 +0300

    Create, select, verify

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,6 +25,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
+    private static void verifyUserPIN(CadClientInterface cad) throws IOException, CadTransportException {
+        Apdu apdu;// verify PIN
+        apdu = new Apdu();
+        apdu.command = new byte[]{(byte) 0x80, (byte) 0x20, 0x00, 0x00};
+        apdu.setDataIn(new byte[]{0x01, 0x02, 0x03, 0x04, 0x05});
+        cad.exchangeApdu(apdu);
+
+        //Verify user pin
+        System.out.println("Verify user pin: 0x80 0x20 0x00 0x00 0x05 0x01 0x02 0x03 0x04 0x05 0x7F");
+        System.out.println(apdu);
+    }
+
+    private static void verifyInvalidUserPIN(CadClientInterface cad) throws IOException, CadTransportException {
+        Apdu apdu;// verify PIN
+        apdu = new Apdu();
+        apdu.command = new byte[]{(byte) 0x80, (byte) 0x20, 0x00, 0x00};
+        apdu.setDataIn(new byte[]{0x01, 0x02, 0x03, 0x04, 0x06});
+        cad.exchangeApdu(apdu);
+
+        System.out.println("Verify invalid user pin: 0x80 0x20 0x00 0x00 0x04 0x01 0x03 0x02 0x66 0x7F");
+        System.out.println(apdu);
+    }
+
     public static void main(String[] args) throws IOException, CadTransportException, GeneralSecurityException {
         CadClientInterface cad;
         Socket sock;
@@ -33,6 +56,32 @@ public class Main {
         OutputStream os = sock.getOutputStream();
         cad = CadDevice.getCadClientInstance(CadDevice.PROTOCOL_T1, is, os);
         cad.powerUp();
+
+        // create wallet
+        Apdu apdu = new Apdu();
+        apdu.command = new byte[]{(byte) 0x80, (byte) 0xB8, 0x00, 0x00};
+        apdu.setDataIn(new byte[]{0x0a, (byte) 0xa0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0x0c, 0x06, 0x01, 0x08, 0x00,
+                0x00, 0x05, 0x01, 0x02, 0x03, 0x04, 0x05});
+        cad.exchangeApdu(apdu);
+
+        System.out.println("Create wallet applet: 0x80 0xB8 0x00 0x00 0x14 0x0a 0xa0 0x0 0x0 0x0 0x62 0x3 0x1 0xc 0x6 0x1 0x08 0x0 0x0 0x05 0x01 0x02 0x03 0x04 0x05 0x7F");
+        System.out.println(apdu);
+
+        // select wallet
+        apdu = new Apdu();
+        apdu.command = new byte[]{0x00, (byte) 0xA4, 0x04, 0x00};
+        apdu.setDataIn(new byte[]{(byte) 0xa0, 0x0, 0x0, 0x0, 0x62, 0x3, 0x1, 0xc, 0x6, 0x1});
+        cad.exchangeApdu(apdu);
+
+        System.out.println("Select wallet: 0x00 0xA4 0x04 0x00 0x0a 0xa0 0x0 0x0 0x0 0x62 0x3 0x1 0xc 0x6 0x1 0x7F");
+        System.out.println(apdu);
+
+        // verify user pin
+        verifyUserPIN(cad);
+
+        // verify invalid user pin
+        verifyInvalidUserPIN(cad);
+
         cad.powerDown(true);
     }
 }
