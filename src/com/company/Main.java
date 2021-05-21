438515ece6d9b5f29b107abc94b6e0aeb9c513dc
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-05-22 02:35:24 +0300

    Login + saved data to smartcard (id, pin, contest codes)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,10 @@ import com.sun.javacard.apduio.Apdu;
 import com.sun.javacard.apduio.CadClientInterface;
 import com.sun.javacard.apduio.CadDevice;
 import com.sun.javacard.apduio.CadTransportException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.io.*;
+import java.util.Scanner;
+
+
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -25,10 +23,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.*;
 
+
+
 public class Main {
+    public static String id, pin;
+    public static int ML_contest, AI_contest, Python_contest, Crypto_contest, NET_contest = 0;
     private static final String capFilePath =
             "C:\\Program Files (x86)\\Oracle\\Java Card Development Kit Simulator 3.1.0\\samples\\classic_applets\\Wallet\\applet\\apdu_scripts\\cap-Wallet.script";
-    int[] contest_codes = new int[]{10, 20, 30, 40, 50};
+
     public static void main(String[] args) throws IOException, CadTransportException, GeneralSecurityException {
         runServer(); //run server without eclipse
 
@@ -107,9 +109,6 @@ public class Main {
 
         System.out.println();
 
-
-
-
         // create wallet
         Apdu apdu = new Apdu();
         apdu.command = new byte[]{(byte) 0x80, (byte) 0xB8, 0x00, 0x00};
@@ -132,11 +131,33 @@ public class Main {
         System.out.println("SW1: " +  byteToHexByte(apdu.getSw1Sw2()[0])+ " SW2: " +  byteToHexByte(apdu.getSw1Sw2()[1]));
         System.out.println();
 
-        //verificam daca utilizatorul e student sau profesor
-//        Scanner sc=new Scanner(System.in);
-//        System.out.print("Ești un student? (da/nu)");
-//        String ans = sc.nextLine();
+        // LOGIN STUDENT
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ești un student? (da/nu)\n");
+        System.out.print("R: ");
+        String ans = sc.nextLine();
+        //System.out.print(ans);
 
+        if(ans.equals("da"))
+            login();
+
+        System.out.println("ID student logat: " + id);
+        System.out.println("PIN student logat: " + pin);
+
+        System.out.println("ML code contest: " + ML_contest);
+        System.out.println("AI code contest: " + AI_contest);
+        System.out.println("Python code contest: " + Python_contest);
+        System.out.println("Crypto code contest: " + Crypto_contest);
+        System.out.println(".NET code contest: " + NET_contest);
+
+        System.out.println();
+
+        //LA CE CONCURS VREA STUDENTUL SA PARTICIPE?
+//        sc = new Scanner(System.in);
+//        System.out.print("La ce concursuri vrei să participi? \n");
+//        System.out.print("R: ");
+//        ans = sc.nextLine();
+//        System.out.println(ans);
 
         // a. Din aplicatia Terminal studentul introduce PIN-ul care este trimis la Java Card Applet pentru validare
         // verify user pin
@@ -149,8 +170,8 @@ public class Main {
 
 
         //b. Daca PIN-ul este validat, comisia alege din meniul aplicatiei Terminal, codul concursului corespunzator si introduce punctajul pe card.
-        choose_code(cad);
-        cad.exchangeApdu(apdu);
+//        choose_code(cad);
+//        cad.exchangeApdu(apdu);
 
         cad.powerDown(true);
     }
@@ -166,6 +187,63 @@ public class Main {
         return helper;
     }
 
+    public static void login() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introdu ID-ul: ");
+        id = sc.nextLine();
+        //System.out.println(id);
+
+        System.out.print("Introdu PIN-ul: ");
+        pin = sc.nextLine();
+        System.out.println();
+        //System.out.println(pin);
+
+        sc.close();  //closes the scanner
+        //save in Terminal data for specific student
+
+        String[] arr = {};
+        //parsing a CSV file into Scanner class constructor
+        try (Scanner csv = new Scanner(new File("Studenti.csv"))) {
+            csv.useDelimiter(",|\\r\\n");  //sets the delimiter pattern
+            while (csv.hasNext())  //returns a boolean value
+            {
+                //System.out.print(csv.next());
+                arr = Arrays.copyOf(arr, arr.length + 1);
+                arr[arr.length - 1] = csv.next();
+                //System.out.println(); //find and returns the next complete token from this scanner
+            }
+
+            //System.out.println("elementul 7: " + arr[16]);
+            csv.close();  //closes the scanner
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //Parcurg vectorul de date, caut studentul si codurile materiilor
+        String pathToCsv = "F:\\Github\\Student-Contest-Card\\Studenti.csv";
+        for(int i = 0; i< arr.length; i++)
+            //System.out.println("arr[" + i + "]: " + arr[i] + " ");
+            if(arr[i].equals(id))
+                if(arr[i+1].equals(pin))
+                {
+                    //int ML_mark = Integer.parseInt(arr[i+5]);
+                    ML_contest = Integer.parseInt(arr[i+7]);
+                    //System.out.println(ML_contest);
+                    AI_contest = Integer.parseInt(arr[i+16]);
+                    //System.out.println(AI_contest);
+                    Python_contest = Integer.parseInt(arr[i+25]);
+                    //System.out.println(Python_contest);
+                    Crypto_contest = Integer.parseInt(arr[i+34]);
+                    //System.out.println(Crypto_contest);
+                    NET_contest = Integer.parseInt(arr[i+43]);
+                    //System.out.println(NET_contest);
+                    break;
+                }
+             else
+                System.out.println("ID sau PIN gresit");
+    }
+
+
     private static void runServer() {
         try {
             String crefFilePath = "c:\\Program Files (x86)\\Oracle\\Java Card Development Kit Simulator 3.1.0\\bin\\cref.bat";
@@ -174,21 +252,6 @@ public class Main {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
-
-    private static void choose_code(CadClientInterface cad) throws IOException, CadTransportException {
-        Apdu apdu;
-        apdu = new Apdu();
-        apdu.command = new byte[]{(byte) 0x80, (byte) 0x51, 0x00, 0x00};
-        apdu.setDataIn(new byte[]{0x01, 0x02, 0x03, 0x04, 0x05});
-        cad.exchangeApdu(apdu);
-
-        //Verify user pin
-        System.out.println("Verify choose code: 0x80 0x20 0x00 0x00 0x05 0x01 0x02 0x03 0x04 0x05 0x7F");
-        System.out.println(apdu);
-        System.out.println("SW1: " +  byteToHexByte(apdu.getSw1Sw2()[0])+ " SW2: " +  byteToHexByte(apdu.getSw1Sw2()[1]));
-        //System.out.println(apdu.getSw1Sw2());
-        System.out.println();
     }
 
 
@@ -220,6 +283,5 @@ public class Main {
         //System.out.println(apdu.getSw1Sw2());
         System.out.println();
     }
-
 
 }
